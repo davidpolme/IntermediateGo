2f17b02a99c5d1ecf656b6e161fabeec7862d6d0
david.polania <david.polania@rappi.com>
2021-09-20 14:08:34 -0500

    Repaso general: GoRoutines y apuntadores

--- main.go
+++ main.go
@@ -2,25 +2,27 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"time"
 )
 
 func main() {
-	// var x int
-	// x = 8
-	// y := 7
+	var x int
+	x = 8
+	y := 7
 
-	// fmt.Println(x)
-	// fmt.Println(y)
-	// myvalue, err := strconv.ParseInt("NaN", 0, 64)
-	// if err != nil {
-	// 	fmt.Printf("Pero que ha pasao :o : %v\n", err)
-	// } else {
-	// 	fmt.Println("Todo bien, todo correcto. Y yo que me alegro :') :", myvalue)
-	// }
+	fmt.Println(x)
+	fmt.Println(y)
+	myvalue, err := strconv.ParseInt("NaN", 0, 64)
+	if err != nil {
+		fmt.Printf("Pero que ha pasao :o : %v\n", err)
+	} else {
+		fmt.Println("Todo bien, todo correcto. Y yo que me alegro :') :", myvalue)
+	}
 
-	// m := make(map[string]int)
-	// m["Key"] = 6
-	// fmt.Println(m["Key"])
+	m := make(map[string]int)
+	m["Key"] = 6
+	fmt.Println(m["Key"])
 	s := []string{"Abra", "Ka", "Dabra"}
 	for index, value := range s {
 		fmt.Printf("%d : %s\n", index, value)
@@ -30,4 +32,22 @@ func main() {
 	for index, value := range s {
 		fmt.Printf("%d : %s\n", index, value)
 	}
+
+	c := make(chan int)
+	go doSomething(c)
+	<-c
+
+	g := 25
+
+	fmt.Println(g)
+
+	h := &g
+
+	fmt.Println(*h)
+}
+
+func doSomething(c chan int) {
+	time.Sleep(time.Second * 1)
+	fmt.Println("Done")
+	c <- 1
 }
