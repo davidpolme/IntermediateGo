0d4929450aefc3a5b1c55d388aeb0fe0af7bcd8b
david.polania <david.polania@rappi.com>
2021-09-21 08:47:31 -0500

    metodos y funciones

--- main.go
+++ main.go
@@ -1,53 +1,32 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-	"time"
-)
+import "fmt"
 
-func main() {
-	var x int
-	x = 8
-	y := 7
-
-	fmt.Println(x)
-	fmt.Println(y)
-	myvalue, err := strconv.ParseInt("NaN", 0, 64)
-	if err != nil {
-		fmt.Printf("Pero que ha pasao :o : %v\n", err)
-	} else {
-		fmt.Println("Todo bien, todo correcto. Y yo que me alegro :') :", myvalue)
-	}
-
-	m := make(map[string]int)
-	m["Key"] = 6
-	fmt.Println(m["Key"])
-	s := []string{"Abra", "Ka", "Dabra"}
-	for index, value := range s {
-		fmt.Printf("%d : %s\n", index, value)
-	}
-	s = append(s, ":)")
-	fmt.Println("___________________________________")
-	for index, value := range s {
-		fmt.Printf("%d : %s\n", index, value)
-	}
-
-	c := make(chan int)
-	go doSomething(c)
-	<-c
-
-	g := 25
-
-	fmt.Println(g)
-
-	h := &g
-
-	fmt.Println(*h)
+type Employee struct {
+	id   int
+	name string
 }
 
-func doSomething(c chan int) {
-	time.Sleep(time.Second * 1)
-	fmt.Println("Done")
-	c <- 1
+func (e *Employee) SetId(id int) {
+	e.id = id
+}
+
+func (e *Employee) SetName(name string) {
+	e.name = name
+}
+
+func (e *Employee) GetId() int {
+	return e.id
+}
+
+func (e *Employee) GetName() string {
+	return e.name
+}
+
+func main() {
+	e := Employee{}
+	e.SetId(2)
+	e.SetName("David")
+	fmt.Printf("%v\n", e)
+	fmt.Printf("id: %d, nombre: %s\n", e.GetId(), e.GetName())
 }
