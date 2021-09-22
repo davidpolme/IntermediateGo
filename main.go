83f8f34c26c0afd30791e1ce2af144b7a9cb711b
davidpolme <david.polania3@gmail.com>
2021-09-22 11:24:47 -0500

    Funciones variadicas y retornos con nombre

--- main.go
+++ main.go
@@ -1,17 +1,32 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
+
+func suma(values ...int) int {
+	total := 0
+	for _, num := range values {
+		total += num
+	}
+
+	return total
+}
+
+func printNames(names ...string) {
+	for _, name := range names {
+		fmt.Println(name)
+	}
+}
+
+func getValues(x int) (double int, triple int, quad int) {
+	double = 2 * x
+	triple = 3 * x
+	quad = 4 * x
+	return
+}
 
 func main() {
-	c := make(chan int)
-	go func() {
-		fmt.Println("Starting Function")
-		time.Sleep(2 * time.Second)
-		fmt.Println("End")
-		c <- 1
-	}()
-	<-c
+	fmt.Println(suma(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+	printNames("David", "Alba", "Lucia")
+
+	fmt.Println(getValues(3))
 }
