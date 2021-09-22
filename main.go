5fcf176d1278a2319d5bb8f9064efd6bf8f2cf41
davidpolme <david.polania3@gmail.com>
2021-09-22 15:10:00 -0500

    Go Modules

--- main.go
+++ main.go
@@ -1,32 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-func suma(values ...int) int {
-	total := 0
-	for _, num := range values {
-		total += num
-	}
+	"github.com/donvito/hellomod"
 
-	return total
-}
-
-func printNames(names ...string) {
-	for _, name := range names {
-		fmt.Println(name)
-	}
-}
-
-func getValues(x int) (double int, triple int, quad int) {
-	double = 2 * x
-	triple = 3 * x
-	quad = 4 * x
-	return
-}
+	himodV2 "github.com/donvito/hellomod/v2"
+)
 
 func main() {
-	fmt.Println(suma(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
-	printNames("David", "Alba", "Lucia")
-
-	fmt.Println(getValues(3))
+	fmt.Println("Hello World!")
+	hellomod.SayHello()
+	himodV2.SayHello("Holaas")
 }
