71da0e9503423b2cfc995099288ce1d7a9596e94
davidpolme <david.polania3@gmail.com>
2021-09-26 16:07:50 -0500

    Buffered channels como semaforos

--- main.go
+++ main.go
@@ -7,18 +7,20 @@ import (
 )
 
 func main() {
+	c := make(chan int, 1)
 	var wg sync.WaitGroup
 	for i := 0; i < 10; i++ {
+		c <- 1
 		wg.Add(1)
-		go doSomething(i, &wg)
+		go doSomething(i, &wg, c)
 	}
-
 	wg.Wait()
 }
 
-func doSomething(i int, wg *sync.WaitGroup) {
+func doSomething(i int, wg *sync.WaitGroup, c chan int) {
 	defer wg.Done()
-	fmt.Printf("Started %d\n", i)
-	time.Sleep(2 * time.Second)
-	fmt.Printf("End %d\n", i)
+	fmt.Printf("Id %d started\n", i)
+	time.Sleep(1 * time.Second)
+	fmt.Printf("---------------Id %d finished\n", i)
+	<-c
 }
