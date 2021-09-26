bfe056616970cb9300dd19d542e370c632390535
davidpolme <david.polania3@gmail.com>
2021-09-26 00:26:58 -0500

    TESTING: Implementing MOCKS

--- main_test.go
+++ main_test.go
@@ -22,11 +22,37 @@ func TestGetFulltimeEmployeeByID(t *testing.T) {
 				GetPersonByDNI = func(dni string) (Person, error) {
 					return Person{
 						Name: "John Doe",
-						Age:  34,
+						Age:  35,
 						DNI:  "1",
 					}, nil
 				}
 			},
+			expectedEmployee: FullTymeEmployee{
+				Person: Person{
+					Name: "John Doe",
+					Age:  35,
+					DNI:  "1",
+				},
+				Employee: Employee{
+					Id:       1,
+					Position: "CEO",
+				},
+			},
 		},
 	}
+	originalGetEmployeeByID := GetEmployeeById
+	originalGetPersonByDNI := GetPersonByDNI
+	for _, test := range table {
+		test.mockFunc()
+		ft, err := GetFullTimeEmployeeByID(test.id, test.dni)
+
+		if err != nil {
+			t.Errorf("Error when getting Employee")
+		}
+		if ft.Age != test.expectedEmployee.Age {
+			t.Errorf("Error, got %d expected %d", ft.Age, test.expectedEmployee.Age)
+		}
+	}
+	GetEmployeeById = originalGetEmployeeByID
+	GetPersonByDNI = originalGetPersonByDNI
 }
