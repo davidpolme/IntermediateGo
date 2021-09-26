81902adcef5c0a7dc3b4c937d1845d1e4edabcbe
davidpolme <david.polania3@gmail.com>
2021-09-26 04:27:55 -0500

    buffered & unbuffered channels

--- main.go
+++ main.go
@@ -1,52 +1,13 @@
 package main
 
-import "time"
-
-type Person struct {
-	DNI  string
-	Name string
-	Age  int
-}
-
-type Employee struct {
-	Id       int
-	Position string
-}
-
-type FullTymeEmployee struct {
-	Employee
-	Person
-}
-
-var GetPersonByDNI = func(dni string) (Person, error) {
-	time.Sleep(5 * time.Second)
-	//SELECT * FROM Persona WHERE ...
-	return Person{}, nil
-}
-
-var GetEmployeeById = func(id int) (Employee, error) {
-	time.Sleep(5 * time.Second)
-	//
-	return Employee{}, nil
-}
-
-var GetFullTimeEmployeeByID = func(id int, dni string) (FullTymeEmployee, error) {
-	var ftEmployee FullTymeEmployee
-	e, err := GetEmployeeById(id)
-	if err != nil {
-		return ftEmployee, err
-	}
-
-	ftEmployee.Employee = e
-
-	p, err := GetPersonByDNI(dni)
-	if err != nil {
-		return ftEmployee, err
-	}
-	ftEmployee.Person = p
-
-	return ftEmployee, nil
-}
+import "fmt"
 
 func main() {
+	c := make(chan int, 3)
+
+	c <- 1
+	c <- 2
+
+	fmt.Println(<-c)
+	fmt.Println(<-c)
 }
